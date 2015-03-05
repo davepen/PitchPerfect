13e674f07950b70477e3295dc34cb95484a731cc
David Penaskovic <davepen@rocketmail.com>
2015-03-05 00:03:49 -0800

    First version. recording button, label, and stop button.

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func recordAudio(sender: UIButton)
+    {
+        println("recording in progress")
+        recordingLabel.hidden = false;
+        stopButton.hidden = false;
+    }
+    
+    @IBAction func stopAudio(sender: UIButton)
+    {
+        recordingLabel.hidden = true;
+        stopButton.hidden = true;
+    }
 }
 
