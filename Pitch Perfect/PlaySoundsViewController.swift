da085c47cd90e6fc9eb4d1841d4145af6339f683
David Penaskovic <davepen@rocketmail.com>
2015-03-09 22:20:27 -0700

    Change background colors. Reset player and engine before playing new sound.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -1,40 +1,40 @@
 import UIKit
 import AVFoundation
 
-class PlaySoundsViewController: UIViewController
+class PlaySoundsViewController : UIViewController
 {
-    var audioPlayer: AVAudioPlayer!
-    var receivedAudio: RecordedAudio!
-    var audioEngine: AVAudioEngine!
-    var audioFile: AVAudioFile!
+    var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
-        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.fileAtPathUrl!, error: nil)
+        audioPlayer = AVAudioPlayer(contentsOfURL:receivedAudio.fileAtPathUrl!, error:nil)
         audioPlayer.enableRate = true;
         
         audioEngine = AVAudioEngine()
-        audioFile = AVAudioFile(forReading:receivedAudio.fileAtPathUrl, error: nil)
+        audioFile = AVAudioFile(forReading:receivedAudio.fileAtPathUrl, error:nil)
     }
 
-    @IBAction func playSlow(sender:UIButton)
+    @IBAction func playSlowAudio(sender:UIButton)
     {
-        playWithRate(0.5)
+        playAudioWithRate(0.5)
     }
 
-    @IBAction func playFast(sender:UIButton)
+    @IBAction func playFastAudio(sender:UIButton)
     {
-        playWithRate(2.0)
+        playAudioWithRate(2.0)
     }
     
-    @IBAction func playChipmunk(sender:UIButton)
+    @IBAction func playChipmunkAudio(sender:UIButton)
     {
         playAudioWithPitch(1000)
     }
     
-    @IBAction func playDarthVader(sender:UIButton)
+    @IBAction func playDarthVaderAudio(sender:UIButton)
     {
         playAudioWithPitch(-1000)
     }
@@ -44,9 +44,10 @@ class PlaySoundsViewController: UIViewController
         audioPlayer.stop()
     }
 
-    func playWithRate(rate:Float)
+    func playAudioWithRate(rate:Float)
     {
-        audioPlayer.stop()
+        stopAndReset()
+
         audioPlayer.rate = rate
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
@@ -54,9 +55,7 @@ class PlaySoundsViewController: UIViewController
     
     func playAudioWithPitch(pitch:Float)
     {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAndReset()
 
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
@@ -65,12 +64,19 @@ class PlaySoundsViewController: UIViewController
         changePitchEffect.pitch = pitch
         audioEngine.attachNode(changePitchEffect)
         
-        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        audioEngine.connect(audioPlayerNode, to:changePitchEffect, format:nil)
+        audioEngine.connect(changePitchEffect, to:audioEngine.outputNode, format:nil)
         
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler:nil)
         audioEngine.startAndReturnError(nil)
         
         audioPlayerNode.play()
+    }
+    
+    func stopAndReset()
+    {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
     }
 }
